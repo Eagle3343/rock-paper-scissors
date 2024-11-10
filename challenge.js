eff274a290ad9b05556c94148c81bdca8c987b0a
Eagle3343 <Eagle3343>
2024-11-10 20:02:24 +0400

    code solved

--- challenge.js
+++ challenge.js
@@ -1,2 +1,30 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+function playGame(player1,player2){
+
+    if (player1 ==="scissor" && player2 ==="paper") {
+      return "player 1 wins"
+    } 
+    else if(player1 ==="paper" && player2 ==="rock") {
+      return "player 2 wins"
+    }
+    else if(player1 ==="rock" && player2 ==="scissor") {
+      return "player 1 wins"
+    }
+    
+    else if(player1 ==="scissor" && player2 ==="scissor") {
+      return "tie"
+    
+  }
+  else if(player1 ==="rock" && player2 ==="rock") {
+      return "tie"
+    
+  }
+  }
+  console.log(playGame("paper","rock"));
+  console.log(playGame("scissor","scissor"));
+  console.log(playGame("scissor","paper"));
+  
+  
